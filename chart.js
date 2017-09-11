918aa8d856c3e8dee34561c65a86759bc2b2483c
Andrew Borisenko <seigiard@gmail.com>
2017-09-11 18:52:40 +0300

    UPDATE chart

--- chart.js
+++ chart.js
@@ -1,10 +1,15 @@
-let studentLength = 100;
+let studentLength = undefined;
 
 function doData(value) {
     studentLength = value.feed.entry[value.feed.entry.length-1]['gs$cell'].row-1;
+
     const result = value.feed.entry
         .reduce(parseData, [])
         .map(prepareChart);
+
+    result[0].values.forEach((x, id) => {
+        setRatingByColumn(id)(result);
+    })
 
     timeline = result[0];
 
@@ -32,11 +37,9 @@ function prepareChart(student, id){
     const values = [];
 
     const studentPointsLength = student.values.length;
-    for(let i = 0; i < studentPointsLength; i = i + 2) {
+    for(let i = 0; i < studentPointsLength; i++) {
         values.push({
             points: +student.values[i],
-            place: +student.values[i+1],
-            place_chart: studentLength - student.values[i+1],
         });
     }
 
@@ -55,9 +58,10 @@ function getChartData(result){
 
     const graphs = getChartGraphs(charts);
 
-    const labels = getResultLabels(result);
+    const currentLabels = getCurrentResultLabels(result);
+    const startLabels = getStartResultLabels(result);
 
-    return { values, graphs, labels };
+    return { values, graphs, currentLabels, startLabels };
 }
 
 function getChartValues(charts) {
@@ -80,7 +84,22 @@ function getChartGraphs(charts) {
         }
 
         result.push({
+            "valueAxis": 'start',
             "id":`g${id}`,
+            "balloonText": `<b>[[place_${id}]]</b><br />[[points_${id}]]`,
+            "type": "smoothedLine",
+            "lineThickness": x.place <= 30 ? 1 : 1,
+            "lineAlpha": x.place <= 30 ? 1 : .2,
+            "bullet":"round",
+            "bulletAlpha": x.place <= 30 ? 1 : .5,
+            "bulletSize":2,
+            "bulletBorderAlpha":0,
+            "valueField": `place_chart_${id}`
+        });
+
+        result.push({
+            "valueAxis": 'current',
+            "id":`g2${id}`,
             "balloonText": `<b>[[place_${id}]]</b><br />[[points_${id}]]`,
             "type": "smoothedLine",
             "lineThickness": x.place <= 30 ? 1 : 1,
@@ -95,12 +114,14 @@ function getChartGraphs(charts) {
     }, [])
 }
 
-function getResultLabels(result) {
+function getCurrentResultLabels(result) {
     return result
+        .slice(1)
         .map(x => Object.assign({}, {
             name: x.name,
             place: x.values[x.values.length-1].place
         }))
+        .sort((a, b) => a.place - b.place)
         .reduce((total, x) => {
             const place = studentLength - x.place;
             total[place] = `${x.place}. ${x.name}`;
@@ -108,22 +129,65 @@ function getResultLabels(result) {
         }, [])
 }
 
+function getStartResultLabels(result) {
+    return result
+        .slice(1)
+        .map(x => Object.assign({}, {
+            name: x.name,
+            place: x.values[0].place
+        }))
+        .sort((a, b) => a.place - b.place)
+        .map((x, id) => Object.assign({}, x, { place: id+1 }))
+        .reduce((total, x) => {
+            const place = studentLength - x.place;
+            // console.error(x.place,place, x.name);
+            total[place] = `${x.place}. ${x.name}`;
+            return total;
+        }, [])
+}
+
+function setRatingByColumn(i) {
+    return function(results) {
+        const data = results
+            .slice(1)
+            .map((x, id) => {
+                x.id = id;
+                return x;
+            })
+            .sort((a, b) => b.values[i].points - a.values[i].points)
+            .map((x, id) => {
+                x.values[i].place = id+1;
+                x.values[i].place_chart = studentLength - id;
+                return x;
+            });
+    }
+}
+
 function drawChart(data) {
+    console.log(data);
     var chart = AmCharts.makeChart("chartdiv", {
         "type": "serial",
         "theme": "light",
         autoMargins: false,
         marginTop: 50,
         marginBottom: 20,
-        marginLeft: 0,
+        marginLeft: 180,
         marginRight: 180,
         "dataProvider": data.values,
         "valueAxes": [{
+            id: 'current',
             "axisAlpha": 0,
             "position": "right",
             "gridThickness": 0,
             "axisColor": '#fff',
-            "labelFunction": position => data.labels[position] || '',
+            "labelFunction": position => data.currentLabels[position-1] || '',
+        }, {
+            id: 'start',
+            "axisAlpha": 0,
+            "position": "left",
+            "gridThickness": 0,
+            "axisColor": '#fff',
+            "labelFunction": position => data.startLabels[position-1] || '',
         }],
         "categoryAxis": {
             position: 'top',
