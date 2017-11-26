fea81805361397c2ba2c571115ac857767c2a323
Andrew Borisenko <seigiard@gmail.com>
2017-11-26 23:08:50 +0200

    Disable poem

--- poem.js
+++ poem.js
@@ -1,5 +1,6 @@
 const hours = new Date().getHours();
+const day = new Date().getDay();
 
-if(hours >= 23 || hours < 1) {
+if((hours >= 23 || hours < 1) && day <=5) {
     document.querySelector('.poem').style.display = "block";
 }
